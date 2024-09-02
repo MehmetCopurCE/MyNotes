c26ef85426484929cf931ed245c867df909550ba
Mehmet Copur <111346774+MehmetCopurCE@users.noreply.github.com>
2024-09-02 23:21:55 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo</title>
+</head>
+
+<body>
+<?php
+$books = [
+    [
+        'title' => 'The Adventures of Tom Sawyer',
+        'author' => 'Mark Twain',
+        'price' => 200,
+        'purchaseUrl' => 'https://example.com'
+    ],
+    [
+        'title' => 'The Adventures of Sherlock Holmes',
+        'author' => 'Arthur Conan Doyle',
+        'price' => 250,
+        'purchaseUrl' => 'https://example.com'
+    ],
+    [
+        'title' => 'The Adventures of Huckleberry Finn',
+        'author' => 'Mark Twain',
+        'price' => 300,
+        'purchaseUrl' => 'https://example.com'
+    ]
+];
+?>
+
+<h1>Recommended Books</h1>
+
+<ul>
+    <?php
+    foreach ($books as $book) {
+        echo "<li><strong>Title: </strong>{$book['title']} <br> <strong>Author: </strong> {$book['author']} <br> <strong>Price: </strong> {$book['price']} <br> <strong>Purchase: </strong> <a href='{$book['purchaseUrl']}'>Link</a> </li><br>";
+    }
+    ?>
+</ul>
+
+<h1>Recommended Books Type 2</h1>
+
+<ul>
+    <?php foreach ($books as $book) : ?>
+        <li>
+            <strong>Title: </strong> <?= $book['title']; ?> <br>
+            <strong>Author: </strong> <?php echo $book['author']; ?> <br>
+            <strong>Price: </strong> <?php echo $book['price']; ?> <br>
+            <strong>Purchase: </strong> <a href="<?php echo $book['purchaseUrl']; ?>">Link</a>
+        </li>
+        <br>
+    <?php endforeach; ?>
+</ul>
+</body>
+
+</html>
+
+</html>
