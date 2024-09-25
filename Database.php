c9f2868454a001c1aa7ecb543c3230dee77836cf
Mehmet Copur <111346774+MehmetCopurCE@users.noreply.github.com>
2024-09-25 08:13:26 +0300

    config.php added and Database.php updated

--- Database.php
+++ Database.php
@@ -4,11 +4,15 @@ class Database
 {
     public $dbConnection;
 
-    public function __construct() //Function that runs when the class is instantiated
+    public function __construct($config) //Function that runs when the class is instantiated
     {
+
         // MySql Connection
-        $dsn = "mysql:host=localhost;dbname=PhpDemo"; // database source name
-        $this->dbConnection = new PDO($dsn, "root", "MCopur123"); //php data objects
+        $dsn = "mysql:" . http_build_query($config, '', ';');
+
+        $this->dbConnection = new PDO($dsn, $config['user'], $config['password'], [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ]); //php data objects
     }
 
     public function query($query){
