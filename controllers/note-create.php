b9535d16971143d9db9782c3ad9335cb9f0eddf3
Mehmet Copur <111346774+MehmetCopurCE@users.noreply.github.com>
2024-10-10 10:17:11 +0300

    Validator and string method added and tested successfully!

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,9 +1,27 @@
 <?php
+require('validator.php');
 
 $header = "Create Node!";
 
+$config = require('config.php');
+$db = new Database($config['database']);
+
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
-    //dd($_POST);
+
+    $errors = [];
+    $body = $_POST['body'];
+
+    if(!Validator::string($body, 1, 1000)){
+        $errors['body'] = "The body should be between 1 and 1000 characters";
+    }
+
+
+    if(empty($errors)){
+       $db->query('INSERT INTO notes (body, user_id) VALUES (:body, :user_id)', [
+           'body' => $body,
+           'user_id' => 1
+           ]);
+    }
 }
 
 
