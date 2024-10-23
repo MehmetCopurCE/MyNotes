61919696a2754ec5d099ece71fe8b048a87701e3
Mehmet Copur <111346774+MehmetCopurCE@users.noreply.github.com>
2024-10-23 21:37:47 +0300

    Composer added and tested Successfully!

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ use Core\ValidationException;
 
 const BASE_PATH = __DIR__ . '/../';
 require BASE_PATH . "Core/functions.php";
+require BASE_PATH . "vendor/autoload.php";
 
 session_start();
 
@@ -16,10 +17,10 @@ session_start();
  Return: require base_path("Core{$class}.php")
 */
 
-spl_autoload_register(function ($class) {
-    $class = str_replace('\\', '/', $class);
-    require base_path("/{$class}.php");
-});
+//spl_autoload_register(function ($class) {
+//    $class = str_replace('\\', '/', $class);
+//    require base_path("/{$class}.php");
+//});
 
 require base_path("bootstrap.php");
 
