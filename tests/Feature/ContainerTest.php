522f513a0356cc43a63fb265c71d0cf682ca7efb
Mehmet Copur <111346774+MehmetCopurCE@users.noreply.github.com>
2024-10-23 22:28:36 +0300

    Composer and Pest added and tested successfully!

---
+++ tests/Feature/ContainerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+test('Container Test', function (){
+    $container = new \Core\Container();
+
+    $container->bind('key', function (){
+        return 'value';
+    });
+
+    $result = $container->resolve('key');
+
+    expect($result)->toBe('value');
+});
